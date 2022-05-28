5a09f48b847c0d1e7a25644e21ae9ce20b2e31dd
alexrosenblatt <rosenblatt.alex@gmail.com>
2022-05-28 00:57:17 -0700

    FML

--- cardclass.py
+++ cardclass.py
@@ -6,28 +6,36 @@ class Card:
         self.name = name
         self.value = val
         self.abbrv = abbrv
-        self.power = power        
-    
+        self.power = power     
+
+
     def get_data(self):
+        '''This returns the details of a specific card.'''
         print(f'{self.name},{self.value},{self.abbrv},{self.power}')
 
-    def get_power_string(self):
-        for key,value in c.POWERS.items():
-            if self.power == value:
-                return key
-
     def update_value(self):
+        '''This function is used when building deck to update card values from constant.'''
         for key,value in c.CARD_VALUES.items():
             if self.name == key:
                 self.value = value
     
     def update_abbrv(self):
+        '''This function is used when building deck to update abbreviation values from constant.'''
         for key,value in c.CARD_ABBRV.items():
             if self.name == key:
                 self.abbrv = value
     
     def update_powers(self):
+        '''This function is used when building deck to update power values from constant.'''
         for key,value in c.CARD_POWERS.items():
             if self.name == key:
                 self.power = value
 
+
+    def get_power_string(self):
+        '''This function returns the string related to the objects power.'''
+        for key,value in c.POWERS.items():
+            if self.power == value:
+                return key
+
+    
